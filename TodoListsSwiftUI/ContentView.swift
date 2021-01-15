0b24ff9cf244373212697e6bc44d23113fbb6fbd
Fernando Martin Ortiz <ortizfernandomartin@gmail.com>
2021-01-15 12:04:04 -0300

    Initial Commit

---
+++ TodoListsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoListsSwiftUI
+//
+//  Created by Fernando Martín Ortiz on 15/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodoListsSwiftUI/TodoListsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListsSwiftUIApp.swift
+//  TodoListsSwiftUI
+//
+//  Created by Fernando Martín Ortiz on 15/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoListsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
